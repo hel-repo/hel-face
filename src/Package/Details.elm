53de136ed68f1bc26cf558b46825f7d13e4c1c1d
MoonlightOwl <murky.owl@gmail.com>
2016-10-26 18:56:43 +0200

    Add tag chips to details page

--- src/Package/Details.elm
+++ src/Package/Details.elm
@@ -2,13 +2,15 @@ module Package.Details exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (class)
-import List exposing (head)
+import List exposing (head, map)
 import String exposing (join)
 
 import Markdown
 
 import Material.Card as Card
+import Material.Chip as Chip
 import Material.Color as Color
+import Material.Elevation as Elevation
 import Material.Options as Options exposing (cs)
 import Material.Spinner as Loading
 
@@ -20,6 +22,10 @@ white : Options.Property c m
 white =
   Color.text Color.white
 
+chip : String -> Html Msg
+chip str =
+  Chip.span [] [ Chip.content [] [ text str ] ]
+
 view : PackageListData -> Html Msg
 view data =
   if data.loading then
@@ -30,17 +36,18 @@ view data =
   else
     case head data.packages of
       Just package ->
-       div
-         [ class "page" ]
-         [ Card.view
-           [ ]
-           [ Card.title
-             [ cs "card-title" ]
-             [ Card.head [ white ] [ text package.name ]
-             , Card.subhead [ ] [ text ("by " ++ ( join ", " package.authors )) ]
-             ]
-           , Card.text [ white ] [ Markdown.toHtml [] package.description ]
-           ]
-         ]
+        div
+          [ class "page" ]
+          [ Card.view
+            [ Elevation.e2 ]
+            [ Card.title
+              [ ]
+              [ Card.head [ white ] [ text package.name ]
+              , Card.subhead [ ] [ text ("by " ++ ( join ", " package.authors )) ]
+              ]
+              , Card.text [ white ] [ Markdown.toHtml [] package.description ]
+              , Card.menu [ ] ( map chip package.tags )
+            ]
+          ]
       Nothing ->
-       text "404: Package not found!"
+        text "404: Package not found!"
