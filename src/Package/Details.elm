12911297bb905a2720d085bbfa66738b8e17ecc6
MoonlightOwl <murky.owl@gmail.com>
2016-10-29 22:15:05 +0200

    Add screenshots slider

--- src/Package/Details.elm
+++ src/Package/Details.elm
@@ -1,12 +1,13 @@
 module Package.Details exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (class, href)
-import List exposing (head, map)
+import Html.Attributes exposing (class, href, src)
+import List exposing (head, map, isEmpty)
 import String exposing (join)
 
 import Markdown
 
+import Material.Button as Button
 import Material.Card as Card
 import Material.Chip as Chip
 import Material.Color as Color
@@ -14,20 +15,52 @@ import Material.Elevation as Elevation
 import Material.Grid as Grid
 import Material.Icon as Icon
 import Material.List as Lists
-import Material.Options as Options exposing (cs)
+import Material.Options as Options exposing (cs, css)
 import Material.Spinner as Loading
 import Material.Tabs as Tabs
+import Material.Typography as Typo
 
 import Base.Messages exposing (Msg(..))
 import Base.Models exposing (materialModel)
 import Package.Messages as PMsg
-import Package.Models exposing (PackageListData, Version, PkgVersionFile, PkgVersionDependency)
+import Package.Models exposing
+  ( PackageListData
+  , Package, Version, PkgVersionFile, PkgVersionDependency
+  )
 import Base.Tools exposing ((!!))
 
 
 white : Options.Property c m
 white =
   Color.text Color.white
+
+
+screensCard : Package -> Html Msg
+screensCard package =
+  if not (isEmpty package.screenshots) then
+    case head package.screenshots of
+      Just screen ->
+        Card.view
+          [ Elevation.e2, cs "screen-card" ]
+          [ Card.text
+              [ cs "screen-img" ]
+              [ img [ src screen.url ] [ ] ]
+          , Card.actions
+              [ Card.border ]
+              [ Button.render Mdl [1,0] materialModel
+                  [ Button.ripple, Button.accent ]
+                  [ text "<" ]
+              , Button.render Mdl [1,1] materialModel
+                  [ Button.ripple, Button.accent ]
+                  [ text ">" ]
+              , Options.styled span
+                  [ Typo.body1, white, cs "screen-desc" ]
+                  [ text screen.description ]
+              ]
+          ]
+      Nothing -> div [ ] [ ]
+  else
+    div [ ] [ ]
 
 
 chip : String -> Html Msg
@@ -57,8 +90,8 @@ versionDesc version =
   div [ class "ver-changes" ] [ Markdown.toHtml [] version.changes ]
 
 
-row : PkgVersionFile -> Html Msg
-row file =
+file : PkgVersionFile -> Html Msg
+file file =
   Lists.li []
     [ Lists.content []
       [ span [ class "list-icon" ] [ Lists.icon "insert_drive_file" [ Icon.size18 ] ]
@@ -67,6 +100,20 @@ row file =
       ]
     ]
 
+files : Version -> Html Msg
+files version =
+  div
+    [ class "files" ]
+    ( case version.files of
+        x::_ ->
+          [ text "Files:"
+          , div [ class "list-of-cards" ]
+              [ Lists.ul [ ] (map file version.files) ]
+          ]
+        [ ] ->
+          [ text "No files." ]
+    )
+
 
 dependencies : Version -> Html Msg
 dependencies version =
@@ -74,25 +121,86 @@ dependencies version =
     x::_ ->
       div [ class "dep-block list-of-cards" ]
         [ text "Depends on:"
-        , Lists.ul []
+        , Lists.ul [ ]
             ( map
-                (\d ->
-                    (Lists.li
-                        [ Lists.withSubtitle ]
-                        [ Lists.content []
-                            [ span [ class "list-icon" ] [ Lists.icon "folder" [ Icon.size18 ] ]
-                            , a [ href ("#packages/" ++ d.name) ] [ text d.name ]
-                            , Lists.subtitle []
-                                [ span [ class "list-cell" ] [ text d.version ] ]
-                            ]
-                        ]
-                    )
+                ( \d ->
+                  ( Lists.li
+                      [ Lists.withSubtitle ]
+                      [ Lists.content [ ]
+                          [ span [ class "list-icon" ] [ Lists.icon "folder" [ Icon.size18 ] ]
+                          , a [ href ("#packages/" ++ d.name) ] [ text d.name ]
+                          , Lists.subtitle [ ]
+                              [ span [ class "list-cell" ] [ text d.version ] ]
+                          ]
+                      ]
+                  )
                 )
                 version.depends
             )
         ]
     [ ] ->
       div [ class "dep-block" ] [ text "No dependencies." ]
+
+
+detailsCard : PackageListData -> Package -> Html Msg
+detailsCard data package =
+  Card.view
+    [ Elevation.e2 ]
+    [ Card.title [ Card.border ]
+        [ Card.head [ white ] [ text package.name ]
+        , Card.subhead [ ]
+            [ span [ class "card-subtitle-icon" ] [ Icon.view "person" [ Icon.size18 ] ]
+            , span [ class "align-top" ] [ text ( join ", " package.authors ) ]
+            , span [ class "card-subtitle-icon card-license" ] [ Icon.view "copyright" [ Icon.size18 ] ]
+            , span [ class "align-top" ] [ license package.license ]
+            ]
+        ]
+    , Card.text [ white ] [ Markdown.toHtml [] package.description ]
+    , Card.menu [ ] ( map chip package.tags )
+    , Card.actions [ white, cs "version-tabs" ]
+        [ Tabs.render Mdl [0] materialModel
+            [ Tabs.ripple
+            , Tabs.onSelectTab (\num -> PackageMsg (PMsg.GoToVersion num))
+            , Tabs.activeTab data.version
+            ]
+            ( map versionLabel package.versions )
+            [ case package.versions !! data.version of
+                Just version ->
+                  div
+                    [ class "page" ]
+                    [ versionDesc version
+                    , Grid.grid [ ]
+                        [ Grid.cell
+                            [ Grid.size Grid.Desktop 6
+                            , Grid.size Grid.Tablet 8
+                            , Grid.size Grid.Phone 4
+                            ]
+                            [ files version ]
+                        , Grid.cell
+                            [ Grid.size Grid.Desktop 6
+                            , Grid.size Grid.Tablet 8
+                            , Grid.size Grid.Phone 4
+                            ]
+                            [ dependencies version ]
+                        ]
+                    ]
+                Nothing ->
+                  div [ class "error" ] [ text "Wrong version code!" ]
+            ]
+        ]
+    ]
+
+
+notFoundCard : Html Msg
+notFoundCard =
+  Card.view
+    [ Elevation.e2 ]
+    [ Card.title [ ] [ Card.head [ white ] [ text "Nothing found!" ] ]
+    , Card.text [ white ]
+        [ div [ ] [ text "No packages with this name was found in repository." ]
+        , div [ ] [ text "Check the spelling, or try different name, please." ]
+        ]
+    ]
 
 
 view : PackageListData -> Html Msg
@@ -103,73 +211,13 @@ view data =
       , cs "spinner"
       ]
   else
-    case head data.packages of
-      Just package ->
-        div
-          [ class "page" ]
-          [ Card.view
-            [ Elevation.e2 ]
-            [ Card.title [ ]
-              [ Card.head [ white ] [ text package.name ]
-              , Card.subhead [ ]
-                [ span [ class "card-subtitle-icon" ] [ Icon.view "person" [ Icon.size18 ] ]
-                , span [ class "align-top" ] [ text ( join ", " package.authors ) ]
-                , span [ class "card-subtitle-icon card-license" ] [ Icon.view "copyright" [ Icon.size18 ] ]
-                , span [ class "align-top" ] [ license package.license ]
-                ]
-              ]
-            , Card.text [ white ] [ Markdown.toHtml [] package.description ]
-            , Card.menu [ ] ( map chip package.tags )
-            , Card.actions [ white, cs "version-tabs" ]
-              [ Tabs.render Mdl [0] materialModel
-                [ Tabs.ripple
-                , Tabs.onSelectTab (\num -> PackageMsg (PMsg.GoToVersion num))
-                , Tabs.activeTab data.version
-                ]
-                ( map versionLabel package.versions )
-                [ case package.versions !! data.version of
-                    Just version ->
-                      div [ class "page" ]
-                        [ versionDesc version
-                        , Grid.grid []
-                            [ Grid.cell
-                                [ Grid.size Grid.Desktop 6
-                                , Grid.size Grid.Tablet 8
-                                , Grid.size Grid.Phone 4
-                                ]
-                                [ div [ class "files" ]
-                                    ( case version.files of
-                                        x::_ ->
-                                          [ text "Files:"
-                                          , div [ class "list-of-cards" ]
-                                              [ Lists.ul [] (map row version.files) ]
-                                          ]
-                                        [ ] ->
-                                          [ text "No files." ]
-                                    )
-                                ]
-                            , Grid.cell
-                                [ Grid.size Grid.Desktop 6
-                                , Grid.size Grid.Tablet 8
-                                , Grid.size Grid.Phone 4
-                                ]
-                                [ dependencies version ]
-                            ]
-                        ]
-                    Nothing -> div [ class "error" ] [ text "Wrong version code!" ]
-                ]
-              ]
+    div
+      [ class "page" ]
+      ( case head data.packages of
+          Just package ->
+            [ screensCard package
+            , detailsCard data package
             ]
-          ]
-      Nothing ->
-        div
-          [ class "page" ]
-          [ Card.view
-            [ Elevation.e2 ]
-            [ Card.title [ ] [ Card.head [ white ] [ text "Nothing found!" ] ]
-            , Card.text [ white ]
-              [ div [ ] [ text "No packages with this name was found in repository." ]
-              , div [ ] [ text "Check the spelling, or try different name, please." ]
-              ]
-            ]
-          ]
+          Nothing ->
+            [ notFoundCard ]
+      )
